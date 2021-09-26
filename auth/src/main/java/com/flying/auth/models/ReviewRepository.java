25ab772c9befa6f2f681d676e806c67c697f93ec
Diego Arroyo <arroyodiego6@gmail.com>
2021-09-25 19:22:00 -0500

    signin and signup implemented and no tested yet

---
+++ auth/src/main/java/com/flying/auth/models/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.flying.auth.models;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
+}
